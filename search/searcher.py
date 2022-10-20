7528274c2cee5fad32759519edc8390e3334c405
jazze13 <75473825+jazze13@users.noreply.github.com>
2022-10-20 14:45:52 +0300

    Add files via upload

---
+++ search/searcher.py
@@ -0,0 +1,19 @@
+# -*- coding: utf8 -*-
+
+import re
+
+def search(keyword):
+    text_file = open('search/static/text.txt')
+    text = ''
+    results = []
+
+    for line in text_file:
+        text += line
+
+    for sentence in re.sub('[!|?|...]', '.', text).split('.'):
+        for word in re.sub('[,|;|:|"|\'|.]', ' ', sentence).split():
+            if keyword.lower() == word.lower():
+                results.append(sentence)
+                break
+
+    return results
