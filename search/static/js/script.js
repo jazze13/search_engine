6dda52cdbe62e9a547b5433ee8afb136a9954393
jazze13 <75473825+jazze13@users.noreply.github.com>
2022-10-27 15:15:48 +0300

    Add files via upload

--- search/static/js/script.js
+++ search/static/js/script.js
@@ -1,59 +1,104 @@
 "use strict";
 
-const theme = document.querySelectorAll('body, .search_bar, .search_bar svg');
+
+// Переключение темы
+document.body.className = localStorage.getItem('theme');
 
 function switch_theme() {
+    const theme = document.querySelectorAll('body, .search_bar, .search_bar svg');
+
     theme.forEach(elem => {
         elem.style.transition = '2s';
     });
-
+    
     document.body.classList.toggle('theme-dark');
     document.body.classList.toggle('theme-light');
     localStorage.setItem('theme', document.body.className);
-
-    theme.forEach(elem => {
-        elem.style.transition = '';
-    });
+    
+    setTimeout(() => {
+        theme.forEach(elem => {
+            elem.style.transition = '';
+        });
+    }, 2000);
 }
 
-document.body.className = localStorage.getItem('theme');
 
-let search_active = false;
+let searchActive = false;
 
-const click_effect = document.querySelectorAll('.click_effect');
-click_effect.forEach(elem => {
+// Ripple
+const clickEffect = document.querySelectorAll('.click_effect');
+clickEffect.forEach(elem => {
     elem.addEventListener('click', function(e) {
-        if ( !search_active && elem.classList.contains('search_bar') ) {
-            let x = e.clientX - e.target.offsetLeft;
-            let y = e.clientY - e.target.offsetTop;
+        if ( elem.classList.contains('search_bar') && searchActive )
+            return;
+        
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
 
-            let ripple = document.createElement('span');
-            ripple.className = 'ripple';
+        let ripple = document.createElement('span');
+        ripple.className = 'ripple';
 
-            ripple.style.top = y + 'px';
-            ripple.style.left = x + 'px';
+        ripple.style.top = y + 'px';
+        ripple.style.left = x + 'px';
 
-            this.appendChild(ripple);
+        this.appendChild(ripple);
 
-            setTimeout(() => {
-                ripple.remove();
-            }, 500);
-        }
+        setTimeout(() => {
+            ripple.remove();
+        }, 500);
     })
 })
 
 
+// Поисковая строка
 const search = document.querySelector('.search');
 
-document.querySelector('.search_bar').addEventListener('click', function() {
+document.querySelector('.search_bar').addEventListener('click', () => {
     search.focus();
     search.style.pointerEvents = 'initial';
-    search_active = true;
+    searchActive = true;
 })
 
 search.onblur = () => {
     if (!search.value) {
         search.style.pointerEvents = 'none';
-        search_active = false;
+        searchActive = false;
     }
 }
+
+
+// Модалки
+document.querySelector('.change_background').addEventListener('click', () => {
+    document.querySelectorAll('.modals_wrapper, .modal').forEach(elem => {
+        elem.style.display = 'flex';
+    })
+})
+
+function closeModals() {
+    document.querySelectorAll('.modals_wrapper, .modal').forEach(elem => {
+        elem.style.display = 'none';
+    })
+}
+
+document.querySelector('.close_modal').addEventListener( 'click', () => {
+    closeModals();
+})
+
+
+// Загрузка Файла для бэкграунда
+// const fileUpload = document.querySelector('.file_upload');
+// const fileUploadLabel = document.querySelector('label[for*=upload]');
+
+// fileUpload.addEventListener('change', e => {
+//     console.log(this);
+//     if (this.files) {
+//         let fileName = e.target.value.split('\\').pop();
+//         fileUploadLabel.innerHTML = fileName;
+//     }
+//     var customBackground = this.files[0];
+// })
+
+// document.querySelector('.custom_backgorund__apply').addEventListener('click', () => {
+//     document.body.style.backgroundImage = customBackground;
+//     closeModals();
+// })
